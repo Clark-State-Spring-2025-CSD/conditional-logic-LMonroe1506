a74e1f768c28dad2c261ba475cd9b1a144a0a71a
LMonroe1506 <monroelois1506@gmail.com>
2025-03-02 18:40:57 -0500

    Update video.py
    Found it!

--- video.py
+++ video.py
@@ -3,3 +3,29 @@
 #Implement all the code shown in the video.
 #Modify the code so the checks from 12 and under for the $8 price tag
 #and 62 and older for the $12 price tag.
+
+print("Welcome to the Menu Ordering System")
+
+CustomerAge = int(input("Please enter your age: "))
+
+price = 0
+
+if CustomerAge <= 10:
+    price = 8
+elif CustomerAge >= 62:
+    price = 9 
+#else:
+#    price = 12
+
+print(f"The cost of your ticket is: ${price}")
+
+drinkYesNo = input("Add a dringk? (Y/N): ")
+
+if drinkYesNo == "Y":
+    smallLarge == "S":
+    if smallLarge == "S":
+        price += 1
+    else:
+        price += 2
+
+print(f"Total is: ${price}")
